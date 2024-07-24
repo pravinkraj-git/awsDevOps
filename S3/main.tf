4daa067237221d10ccf40c36aac5a3fbec939c0d
pravinkraj-git <pravinkraj.cloud@gmail.com>
2024-07-24 13:56:24 +0100

    added s3 terraform code

---
+++ S3/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "my-unique-bucket-name"  # Replace with your unique bucket name
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example_bucket.bucket
+  acl    = "private"
+}
