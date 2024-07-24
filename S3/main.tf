ed178c9b8cfd3608450f37cf79a67071a1a2f0aa
Dhanaraj <vignesh.dhanaraj@sanmina.com>
2024-07-24 19:07:22 +0530

    Revert "region ap_south_1 added"
    This reverts commit 2601b33b86137ffe2d6383fbeeae7918693ea689.

--- S3/main.tf
+++ S3/main.tf
@@ -1,5 +1,6 @@
 provider "aws" {
 <<<<<<< HEAD
+<<<<<<< HEAD
   region = "ap-south-1"  # Specify the AWS region where you want to create the bucket
   alias = "primary"
   region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
@@ -9,6 +10,9 @@ provider "aws" {
   alias = "secondary"
   region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
 >>>>>>> 3ed8b56 (alias included primary and secondary)
+=======
+  region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
+>>>>>>> 67cdc1b (Revert "region ap_south_1 added")
 }
 
 resource "aws_s3_bucket" "example_bucket" {
