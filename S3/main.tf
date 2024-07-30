ee60114e102d1f17db33e34e419f6c77a483e6ef
pravinkraj-git <pravinkraj.cloud@gmail.com>
2024-07-30 14:16:58 +0100

    changes in the s3 main file

--- S3/main.tf
+++ S3/main.tf
@@ -1,18 +1,11 @@
 provider "aws" {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  region = "ap-south-1"  # Specify the AWS region where you want to create the bucket
   alias = "primary"
-  region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
+  region = "ap-south-1"  # Specify the AWS region where you want to create the bucket
 }
 
 provider "aws" {
   alias = "secondary"
   region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
->>>>>>> 3ed8b56 (alias included primary and secondary)
-=======
-  region = "eu-west-2"  # Specify the AWS region where you want to create the bucket
->>>>>>> 67cdc1b (Revert "region ap_south_1 added")
 }
 
 resource "aws_s3_bucket" "example_bucket" {
