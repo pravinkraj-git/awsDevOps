845c1fea55471b2386e7295a64331cd11b858808
anand <vignesh.dhanaraj@yahoo.com>
2024-08-04 23:22:10 +0530

    Reverted the Changes to Initial Commit

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-south-1"  # Specify the AWS region
 }
 
-resource "aws_instance" "learning_instance" {
+resource "aws_instance" "test_instance" {
   ami           = "ami-057084464718466ff"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"  # Instance type
 
